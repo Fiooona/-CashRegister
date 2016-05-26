faa65a1f2e25bd6e7d4c431fd4b04792b2e891c2
Fiooona <845559655@qq.com>
2016-05-26 17:56:54 +0800

    html css

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 /**
  * Created by Fiona 2016/5/20.
  */
+var commodity={
+    barcode:'ITEM000000',
+    name:'可口可乐',
+    unit:'瓶',
+    category:'食品',
+    subCategory:'碳酸饮料',price:3.00
+}
+function print(str){
+
+}
